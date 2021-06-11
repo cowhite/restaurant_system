b0849dff649ebe9f301f6d824dca7c14fe804b68
Balaji <bljd369@gmail.com>
2021-06-11 14:36:51 +0530

    bug fix

--- myapp/mongoconnect.py
+++ myapp/mongoconnect.py
@@ -6,7 +6,7 @@ CODE_TYPE = os.environ.get('CODE_TYPE', "dev")
 def connect_to_mongo():
     if CODE_TYPE == "dev":
         client = MongoClient('localhost', 27017)
-        return client.spark_dev
+        return client.dev_db
     elif CODE_TYPE == "prod":
         client = MongoClient("mongodb://uname:pwd@localhost/admin")
-        return client.spark_prod
+        return client.prod_db
